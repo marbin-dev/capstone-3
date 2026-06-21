9944b4111214e431ff05871e4e07ad8723e1aa49
marbin701 <marvin07012002@gmail.com>
2026-06-21 14:44:13 -0400

    Next anotations added in the CategoriesController:RestController                                                   @RequestMapping("/categories")                                                   @CrossOrigin

--- backend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -15,6 +13,9 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryService categoryService;
