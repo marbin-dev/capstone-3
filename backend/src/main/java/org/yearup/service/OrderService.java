7749b64977f25104681273cc496707081848ccf8
marbin701 <marvin07012002@gmail.com>
2026-06-23 05:18:27 -0400

    OrderLineItem created and logic added

--- backend/src/main/java/org/yearup/service/OrderService.java
+++ backend/src/main/java/org/yearup/service/OrderService.java
@@ -1,5 +1,14 @@
 package org.yearup.service;
 
+import org.springframework.stereotype.Service;
+import org.yearup.repository.OrderRepository;
+
+@Service
 public class OrderService {
-    
+
+    private final OrderRepository orderRepository;
+    private final 
+
+
+
 }
