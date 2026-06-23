33ef30fc8ef810ad0102210d77593d7deccebfa9
marbin701 <marvin07012002@gmail.com>
2026-06-23 03:39:14 -0400

    method added in interface Profile repository, and working on ProfileController

--- backend/src/main/java/org/yearup/controllers/ProfileController.java
+++ backend/src/main/java/org/yearup/controllers/ProfileController.java
@@ -2,10 +2,15 @@ package org.yearup.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
 import org.yearup.service.ProfileService;
 import org.yearup.service.UserService;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/profile")
@@ -19,5 +24,15 @@ public class ProfileController {
     public ProfileController(ProfileService profileService, UserService userService) {
         this.profileService = profileService;
         this.userService = userService;
+    }
+
+    @GetMapping
+    public Profile getProfile(Principal principal){
+        String userName = principal.getName();
+
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        return profileService.get
     }
 }
