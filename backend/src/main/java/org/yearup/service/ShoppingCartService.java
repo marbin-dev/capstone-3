8b3158bc103b39b68e3c3ddeb90c2e3e8c1dd3af
marbin701 <marvin07012002@gmail.com>
2026-06-23 02:50:36 -0400

    Annotation added in the clear cart service

--- backend/src/main/java/org/yearup/service/ShoppingCartService.java
+++ backend/src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,5 +1,6 @@
 package org.yearup.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.yearup.models.CartItem;
 import org.yearup.models.Product;
@@ -67,7 +68,8 @@ public class ShoppingCartService {
         return getByUserId(userId);
     }
 
-    public ShoppingCart clearCart(int userId){
+    @Transactional
+    public ShoppingCart clearCart(int userId) {
         shoppingCartRepository.deleteByUserId(userId);
 
         return getByUserId(userId);
