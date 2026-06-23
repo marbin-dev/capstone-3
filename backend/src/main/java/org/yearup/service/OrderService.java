94e0fd659db63d3312abd154caf9539b135673a6
marbin701 <marvin07012002@gmail.com>
2026-06-23 06:34:47 -0400

    in the OrderService logic added too the check out method

--- backend/src/main/java/org/yearup/service/OrderService.java
+++ backend/src/main/java/org/yearup/service/OrderService.java
@@ -1,10 +1,7 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
-import org.yearup.models.Order;
-import org.yearup.models.OrderLineItem;
-import org.yearup.models.ShoppingCart;
-import org.yearup.models.ShoppingCartItem;
+import org.yearup.models.*;
 import org.yearup.repository.OrderLineItemRepository;
 import org.yearup.repository.OrderRepository;
 
@@ -16,20 +13,28 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderLineItemRepository orderLineItemRepository;
     private final ShoppingCartService shoppingCartService;
+    private final ProfileService profileService;
 
-    public OrderService(OrderRepository orderRepository, OrderLineItemRepository orderLineItemRepository, ShoppingCartService shoppingCartService) {
+    public OrderService(OrderRepository orderRepository, OrderLineItemRepository orderLineItemRepository, ShoppingCartService shoppingCartService, ProfileService profileService) {
         this.orderRepository = orderRepository;
         this.orderLineItemRepository = orderLineItemRepository;
         this.shoppingCartService = shoppingCartService;
+        this.profileService = profileService;
     }
 
     public Order checkout(int userId) {
 
         ShoppingCart cart = shoppingCartService.getByUserId(userId);
+        Profile profile = profileService.getByUserId(userId);
 
         Order order = new Order();
         order.setUserId(userId);
         order.setDate(LocalDateTime.now());
+        order.setAddress(profile.getAddress());
+        order.setCity(profile.getCity());
+        order.setState(profile.getState());
+        order.setZip(profile.getZip());
+        order.setShippingAmount(0);
 
         Order saveOrder = orderRepository.save(order);
 
